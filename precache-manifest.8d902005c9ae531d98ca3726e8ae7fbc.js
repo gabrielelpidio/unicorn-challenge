11efeb0d37cfd7d9c7d8e6769244bf27c1d91f64
Gabriel De Andrade <gabrieldeandradeleal@gmail.com>
2019-07-15 00:07:20 -0400

    Updates

---
+++ precache-manifest.8d902005c9ae531d98ca3726e8ae7fbc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23d0df6ded4024367b3d7f4255a6775a",
+    "url": "/unicorn-challenge/index.html"
+  },
+  {
+    "revision": "62991382f538901ba1a2",
+    "url": "/unicorn-challenge/static/css/main.b918c34c.chunk.css"
+  },
+  {
+    "revision": "ffe21b9017ed0a410367",
+    "url": "/unicorn-challenge/static/js/2.c0b54ba3.chunk.js"
+  },
+  {
+    "revision": "62991382f538901ba1a2",
+    "url": "/unicorn-challenge/static/js/main.fe16b7ff.chunk.js"
+  },
+  {
+    "revision": "729a39fe7d1735d4bbd8",
+    "url": "/unicorn-challenge/static/js/runtime~main.03336328.js"
+  },
+  {
+    "revision": "bb87a23f7bef917d10a77977cdfa8e99",
+    "url": "/unicorn-challenge/static/media/grinch.bb87a23f.png"
+  },
+  {
+    "revision": "fd373597a8d97025d94f819c0dce94d6",
+    "url": "/unicorn-challenge/static/media/menu.fd373597.svg"
+  },
+  {
+    "revision": "a9e05bb684f77a640f6f882a7f89ebde",
+    "url": "/unicorn-challenge/static/media/platziVideo.a9e05bb6.png"
+  }
+]);
